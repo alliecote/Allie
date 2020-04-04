4a8f3e89381f054aa73536122ea3a80560dd5b0e
alliecote <a.cote@ufl.edu>
2020-04-04 22:40:02 +0000

    Site finished

---
+++ my-app/build/precache-manifest.2ac8265f0f3612e3a30cd759a47b2918.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01890d421346300f7ec28a74ea40cbcf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e46a89726d169cc01d8",
+    "url": "/static/css/main.1162162c.chunk.css"
+  },
+  {
+    "revision": "652a599d1a99efccd345",
+    "url": "/static/js/2.a430f49c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a430f49c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e46a89726d169cc01d8",
+    "url": "/static/js/main.6a9b6f06.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  },
+  {
+    "revision": "672d25866c1d1161b187a25e40a7c401",
+    "url": "/static/media/20200315_230550.672d2586.jpg"
+  }
+]);
---
+++ my-app/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './20200315_230550.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome to  <code>Alex Cote's </code> instrumentation lab 8!.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
